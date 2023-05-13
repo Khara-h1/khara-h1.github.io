3ba8c3745edc00a07503b2032051b3646fd4f2df
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2023-05-13 17:02:47 +0200

    Update alert.js

--- poc/alert.js
+++ poc/alert.js
@@ -1,8 +1,3 @@
 document.body.innerHtml="";
 
 alert(document.domain);
-var a=document.createElement("iframe");
-a.src="https://www.nintendo.fr";
-a.name="mainsite";
-a.style = "width: 100%; height: 1024px; border: 0;position: absolute; top: 0; right: 0; bottom: 0: left: 0;"; 
-document.body.appendChild(a);
